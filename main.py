1b0d19a4f641519871b4c0162c0844c85a086619
Felipe Martins <felipe_mmmachado@hotmail.com>
2022-09-01 17:45:09 -0300

    Fixed some bugs

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import random
 
 def Winner(playerNum, computerNum):
-    gapPlayer = 21 - playerNum
-    gapComputer = 21 - computerNum
+    #funcao que calcula o vencedor do jogo, recebendo as pontuacoes de cada jogador
+    gapPlayer = abs(21 - playerNum)
+    gapComputer = abs(21 - computerNum)
+    gapPlayer = abs(gapPlayer)
+    gapComputer = abs(gapComputer)
+
     if gapPlayer < gapComputer:
         return "The player wins!"
     elif gapPlayer > gapComputer:
         return "The house wins!"
-    else:
+    elif gapPlayer == gapComputer:
         return "Draw!"        
-
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
@@ -29,27 +32,24 @@ computerCards.append(random.choice(cards))
 
 #somatorio para armazenar os numeros iniciais
 playerNumber = sum(playerCards)
-
-if playerNumber == 21:
-    print()
-
 computerNumber = sum(computerCards)
 
 #variavel flag para fazer repeticao
-gameStillOn = True
+playerTurn = True
+computerTurn = True
 
-#revela uma só carta da mesa, mas as duas do player
+#revela uma só carta da mesa e duas do player
 print(f"The table got: {computerCards[0]},...")
 print(f"You got: {playerCards}, total: {playerNumber:.0f}")
 
 if playerNumber == 21:
     print("Player BlackJack, the player wins!")
-    gameStillOn = False
+    playerTurn = False
 elif computerNumber == 21:
     print("Computer BlackJack, the table wins!")
-    gameStillOn = False         
+    playerTurn = False         
 
-while gameStillOn == True:
+while playerTurn == True:
     #pergunta para o jogador se ele vai querer mais cartas
     hitAnother = input("Do you want to get another card? (Y/N) ")
 
@@ -63,22 +63,35 @@ while gameStillOn == True:
 
     #para o jogo caso o player ultrapasse 21 pontos
     if playerNumber > 21:
-        print("Busted!")
-        gameStillOn = False
+        print("Busted! The house wins!")
+        playerTurn = False
+        exit()
     #Caso o player tenha exatos 21
     elif playerNumber == 21:
         print(f"21!!! you got {playerCards}, total: {playerNumber:.0f}")
-        gameStillOn = False    
+        playerTurn = False    
 
     #caso o player decida seguir em frente, o computador vai fazer jogadas 
     #nas regras, a casa só joga se tiver com 16 ou menos
     if hitAnother == 'N' or hitAnother == 'n':
         print(f"The table got: {computerCards}, total: {computerNumber}")
-        
-    if computerNumber <= 16:
-            computerCards.append(random.choice(cards))
-        #momento de comparar as pontuações e determinar o vencedor
-        gameStillOn = False
-        print(Winner(playerNumber,computerNumber))    
-          
+        playerTurn = False 
+        computerTurn = True
 
+#agora passa para o turno do computador
+while computerTurn == True:
+    if computerNumber <= 16:
+        computerCards.append(random.choice(cards))
+        computerNumber = sum(computerCards)
+        print(f"The table received another card: {computerCards}, total: {computerNumber}")
+    elif computerNumber >= 17:
+        computerTurn = False
+        if computerNumber > 21:
+            print("The house busted!")
+
+print(computerNumber,playerNumber)
+
+
+if computerTurn==False and playerTurn==False:
+    print(Winner(playerNumber,computerNumber))    
+    #momento de comparar as pontuações e determinar o vencedor
